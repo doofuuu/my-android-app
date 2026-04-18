f3ddfccce1b1a32f862d3f484b7b0702110b757f
Xwy910713 <329409803@qq.com>
2026-04-18 08:22:03 +0800

    Initial commit with Android project and GitHub Actions

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello from GitHub Actions!");
+        setContentView(tv);
+    }
+}
